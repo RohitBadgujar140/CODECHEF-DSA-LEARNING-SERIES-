319035f5ebdf225f416e40654956add29adf60fd
ROHIT BADGUJAR <65911824+RohitBadgujar140@users.noreply.github.com>
2021-10-25 12:52:20 +0530

    Create LAPIN.cpp

---
+++ Complexity Analysis + Basics Warm Up/LAPIN.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	string s;
+	cin>>t;
+	
+	while(t--)
+	{
+	  cin>>s;
+	  int len = s.length();
+	  int left[26] = {0};
+	  int right[26] = {0};
+	  
+	  for(int i=0;i<len/2;i++)
+	  {
+	      int index = s[i] - 'a';
+	      left[index]++;
+	  }
+	  
+	  for(int i=(len+1)/2;i<len;i++)
+	  {
+	      int index = s[i] - 'a';
+	      right[index]++;
+	  }
+	  
+	  int flag = 0;
+	  
+	  for(int i=0;i<26;i++)
+	  {
+	      if(left[i]!=right[i])
+	        flag = 1;
+	  }
+	  
+	  if(flag==0)
+	    cout<<"YES"<<endl;
+	  else
+	    cout<<"NO"<<endl;
+	}
+	
+	return 0;
+}
